a87f0be1b412ba38d6250f08dd181ad1fc47f714
Johncode321 <168512144+Johncode321@users.noreply.github.com>
2024-12-04 21:55:34 +0100

    Update wallet.ts

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -43,8 +43,6 @@ const getDesktopProvider = async (type: WalletProvider) => {
 export const getProvider = async (type: WalletProvider) => {
   console.log(`Getting provider for ${type}`);
   
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
   // Check if we're in an in-app browser
   if (isInAppBrowser()) {
     if (type === 'phantom' && isPhantomBrowser()) {
@@ -76,20 +74,21 @@ export const getProvider = async (type: WalletProvider) => {
   }
 
   // For mobile or if wallet is not installed
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  
   if (isMobile) {
-    // The URL you want to open in the wallet's in-app browser
+    // The URL you want to open in the wallet's browser
     const dappUrl = 'https://test-beta-rouge-19.vercel.app/';
     const encodedUrl = encodeURIComponent(dappUrl);
     const ref = encodeURIComponent(window.location.origin);
     
     if (type === 'solflare') {
-      // Updated Solflare deeplink using 'solflare://' protocol
-      const deeplink = `solflare://v1/browse/${encodedUrl}?ref=${ref}`;
+      // Construct Solflare deeplink according to documentation
+      const deeplink = `https://solflare.com/ul/v1/browse/${encodedUrl}?ref=${ref}`;
       window.location.href = deeplink;
     } else if (type === 'phantom') {
       window.location.href = `https://phantom.app/ul/browse/${encodedUrl}?ref=${ref}`;
     }
-    return null;
   } else {
     // For desktop: open wallet download page if not installed
     const downloadUrls = {
