86ba863620a656e0c6f2877cb977056270384827
Johncode321 <168512144+Johncode321@users.noreply.github.com>
2024-12-04 21:23:37 +0100

    Add files via upload

---
+++ src/contexts/WalletContextProvider.tsx
@@ -0,0 +1,30 @@
+// src/contexts/WalletContextProvider.tsx
+import { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+export const WalletContextProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+    // You can also provide a custom RPC endpoint
+    const endpoint = useMemo(() => clusterApiUrl('mainnet-beta'), []);
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new SolflareWalletAdapter(),
+        ],
+        []
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
