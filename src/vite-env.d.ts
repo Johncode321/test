b1d6bad50be553602f2eb54c821df6261e7ef096
Johncode321 <168512144+Johncode321@users.noreply.github.com>
2025-01-22 18:01:38 +0100

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,33 @@
 /// <reference types="vite/client" />
 
+/// <reference types="vite/client" />
+
 declare module '*.svg' {
   const content: string;
   export default content;
+}
+
+interface Window {
+  phantom?: {
+    solana?: any;
+  };
+  solflare?: any;
+  backpack?: {
+    solana?: any;
+  };
+  exodus?: {
+    solana?: any;
+  };
+  trustwallet?: {
+    solana?: any;
+  };
+  atomic?: {
+    solana?: any;
+  };
+  mathwallet?: {
+    solana?: any;
+  };
+  metamask?: {
+    solana?: any;
+  };
 }
