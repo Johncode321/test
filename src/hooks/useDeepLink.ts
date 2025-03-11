0a96eb1451f7f77494e436471e1f133c5e8fb75e
Johncode321 <168512144+Johncode321@users.noreply.github.com>
2025-03-11 23:46:58 +0100

    Update useDeepLink.ts

--- src/hooks/useDeepLink.ts
+++ src/hooks/useDeepLink.ts
@@ -1,17 +1,13 @@
-// src/hooks/useDeepLink.ts
-
 const createDeepLink = (type: 'phantom' | 'solflare', message: string) => {
   const baseUrl = window.location.href;
   const encodedMessage = encodeURIComponent(message);
   const encodedUrl = encodeURIComponent(baseUrl);
 
-  // Créer un objet avec les paramètres de la transaction
   const params = {
     message: encodedMessage,
     redirect: encodedUrl
   };
 
-  // Construire l'URL de deep link
   const deepLink = type === 'phantom'
     ? `https://phantom.app/ul/v1/signMessage?${new URLSearchParams(params)}`
     : `https://solflare.com/ul/v1/signMessage?${new URLSearchParams(params)}`;
